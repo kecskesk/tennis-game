492ef294b35f2a14714fcb485d9f219a3a36fdf7
Orosz Dávid <orosz.david@sonrisa.hu>
2020-05-27 14:05:31 +0200

    add demo solution

--- TennisGame.js
+++ TennisGame.js
@@ -8,10 +8,58 @@ class TennisGame {
 
     getScore() {
 
-    //TODO implement
+        if (this.hasWinner()) {
+            return this.playerWithHighestScore() + " wins";
+        }
+
+        if (this.hasAdvantage()) {
+            return "Advantage " + this.playerWithHighestScore();
+        }
+
+        if (this.isDeuce())
+            return "Deuce";
+
+        if (this.playerOneScore == this.playerTwoScore) {
+            return this.translateScore(this.playerOneScore) + " all";
+        }
+
+        return this.translateScore(this.playerOneScore) + "," + this.translateScore(this.playerTwoScore);
     }
 
-   
+    isDeuce() {
+        return this.playerOneScore >= 3 && this.playerTwoScore == this.playerOneScore;
+    }
+
+    playerWithHighestScore() {
+        if (this.playerOneScore > this.playerTwoScore) {
+            return this.playerOneName;
+        } else {
+            return this.playerTwoName;
+        }
+    }
+
+    hasWinner() {
+        if (this.playerTwoScore >= 4 && this.playerTwoScore >= this.playerOneScore + 2) {
+            return true;
+        }
+        if (this.playerOneScore >= 4 && this.playerOneScore >= this.playerTwoScore + 2) {
+
+            return true;
+        }
+        return false;
+    }
+
+    hasAdvantage() {
+        if (this.playerTwoScore >= 4 && this.playerTwoScore == this.playerOneScore + 1) {
+            return true;
+        }
+
+        if (this.playerOneScore >= 4 && this.playerOneScore == this.playerTwoScore + 1) {
+            return true;
+        }
+
+        return false;
+    }
 
     playerOneScores() {
         this.playerOneScore++;
@@ -20,6 +68,21 @@ class TennisGame {
     playerTwoScores() {
         this.playerTwoScore++;
     }
+
+    translateScore(score) {
+        switch (score) {
+            case 3:
+                return "Forty";
+            case 2:
+                return "Thirty";
+            case 1:
+                return "Fifteen";
+            case 0:
+                return "Love";
+        }
+    }
+
+
 }
 
 module.exports = TennisGame;
