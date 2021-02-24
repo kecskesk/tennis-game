933a8eb49b2e110202eb9168c9f75004aa1344bf
kecskesk <kecskesk@sonrisa.hu>
2021-02-24 15:01:07 +0000

    final solution

--- TennisGame.js
+++ TennisGame.js
@@ -4,12 +4,36 @@ class TennisGame {
         this.playerTwoScore = 0;
         this.playerOneName = playerOneName;
         this.playerTwoName = playerTwoName;
+        this.scoreMap = {
+            0 : "Love",
+            1 : "Fifteen",
+            2 : "Thirty",
+            3 : "Forty"
+        };
     }
 
     getScore() {
-        //TODO implement
+        if (this.isDeuce()) {
+            return "Deuce";
+        }
+        let player = this.hasAdvantage();
+        if (player) {
+            return 'Advantage ' + player;
+        }
+        let winner = this.winner();
+        if (winner) {
+            return winner + ' wins';
+        }
+
+        const playerOne = this.scoreMap[this.playerOneScore]
+        const playerTwo = this.scoreMap[this.playerTwoScore]
+
+        if (this.playerOneScore === this.playerTwoScore) {
+            return playerOne + " all"
+        } else {
+            return playerOne + "," + playerTwo
+        }
     }
-   
 
     playerOneScores() {
         this.playerOneScore++;
@@ -17,6 +41,34 @@ class TennisGame {
 
     playerTwoScores() {
         this.playerTwoScore++;
+    }
+
+    winner() {
+        let diff = this.playerOneScore - this.playerTwoScore;
+        if (this.playerOneScore >= 4 || this.playerTwoScore >= 4) {
+            if (diff >= 2) {
+                return this.playerOneName;
+            } else if (diff <= -2) {
+                return this.playerTwoName;
+            }
+        }
+        return null;
+    }
+
+    hasAdvantage() {
+        let diff = this.playerOneScore - this.playerTwoScore;
+        if (this.playerOneScore >= 4) {
+            if (diff === 1) {
+                return this.playerOneName;
+            } else if (diff === -1) {
+                return this.playerTwoName;
+            }
+        }
+        return null;
+    }
+
+    isDeuce() {
+        return this.playerOneScore === this.playerTwoScore && this.playerOneScore >= 3;
     }
 }
 
